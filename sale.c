86b7d904e322c51559b289c8c4ad6ef3ab8422fe
6704062610119 <s6704062610119@email.kmutnb.ac.th>
2025-12-09 13:48:56 +0700

    commit 3

--- sale.c
+++ sale.c
@@ -2,13 +2,14 @@
 
 int main(){
 	
+	float vat=107.0/100.0;
 	float price1,price2,price3;
 	scanf("%f",&price1);
-	printf("%.2f\n", price1*95/100);
+	printf("%.2f\n", (price1*vat)*95/100);
 	scanf("%f",&price2);
-	printf("%.2f\n",(price1+price2)*85/100);
+	printf("%.2f\n",(price1*vat+price2*vat)*85/100);
 	scanf("%f",&price3);
-	printf("%.2f\n",(price1+price2+price3)*70/100);
+	printf("%.2f\n",(price1*vat+price2*vat+price3*vat)*70/100);
 	
 	return 0;
 }
